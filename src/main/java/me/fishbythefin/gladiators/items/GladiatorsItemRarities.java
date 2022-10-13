da78f195b0113044cc1bbfe77f37404f6400b423
BuildTools <unconfigured@null.spigotmc.org>
2022-10-13 06:29:04 -0700

    -updated gay ray -changed "cringe" rarity to "meh"

--- src/main/java/me/fishbythefin/gladiators/items/GladiatorsItemRarities.java
+++ src/main/java/me/fishbythefin/gladiators/items/GladiatorsItemRarities.java
@@ -7,6 +7,6 @@ public class GladiatorsItemRarities {
     public static final Component RADICAL = Component.translatable("rarities.gladiators.radical").withStyle(ChatFormatting.GOLD).withStyle(ChatFormatting.BOLD);
     public static final Component TIGHT = Component.translatable("rarities.gladiators.tight").withStyle(ChatFormatting.DARK_PURPLE).withStyle(ChatFormatting.BOLD);
     public static final Component MID = Component.translatable("rarities.gladiators.mid").withStyle(ChatFormatting.BLUE).withStyle(ChatFormatting.BOLD);
-    public static final Component CRINGE = Component.translatable("rarities.gladiators.cringe").withStyle(ChatFormatting.RED).withStyle(ChatFormatting.BOLD);
+    public static final Component MEH = Component.translatable("rarities.gladiators.meh").withStyle(ChatFormatting.GREEN).withStyle(ChatFormatting.BOLD);
     public static final Component DOG_DOODOO = Component.translatable("rarities.gladiators.dog_doodoo").withStyle(ChatFormatting.DARK_RED).withStyle(ChatFormatting.BOLD);
 }
--- src/main/java/me/fishbythefin/gladiators/items/custom/GayRayItem.java
+++ src/main/java/me/fishbythefin/gladiators/items/custom/GayRayItem.java
@@ -1,14 +1,21 @@
 package me.fishbythefin.gladiators.items.custom;
 
+import me.fishbythefin.gladiators.items.GladiatorsItemRarities;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class GayRayItem extends Item {
     public GayRayItem() {
@@ -31,5 +38,12 @@ public class GayRayItem extends Item {
             player.getCooldowns().addCooldown(this, 20);
         }
         return super.use(level, player, interactionHand);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        components.add(Component.translatable("tooltips.gladiators.gay_ray").withStyle(ChatFormatting.GRAY));
+        components.add(GladiatorsItemRarities.MEH);
+        super.appendHoverText(itemStack, level, components, flag);
     }
 }
